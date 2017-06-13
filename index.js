5e13c3a77cb9bd2061d6519c78a9733610741106
Jérémy Derdaele <jeremy.derdaele@gmail.com>
2017-06-13 23:19:42 +0200

    Update transformer to accept string as column definition

--- index.js
+++ index.js
@@ -18,7 +18,16 @@ let output = fs.createWriteStream(target + ".tsv");
 console.log(`Transforming GeoJSON...`)
 
 input.pipe(JSONStream.parse('features.*', x => {
-    let cols = processor.map((fun) => fun(x.properties))
+    let cols = processor.map((col) => {
+        switch(typeof col) {
+            case "function":
+                return col(x.properties);
+            case "string":
+                return x.properties[col];
+            default:
+                throw "Column definition must be a string or a function"
+        }
+    })
     cols.push("SRID=4326;" + wkt.stringify(x.geometry))
     return cols.join("\t") + "\n";
 })).pipe(output);
--- samples/schools.js
+++ samples/schools.js
@@ -1,5 +1,5 @@
 module.exports = [
-    (x) => x.IDTYPE,
+    'IDTYPE',
     (x) => JSON.stringify({
         name: x.appellatio,
         public: x.priv_public == null
